3c580293703724e9a51ecdfee2fd3612eb6cae92
omsi96 <omsi96@gmail.com>
2021-09-18 22:20:03 +0300

    initial commit

---
+++ engine.js
@@ -0,0 +1,69 @@
+/**
+ *   🔴🔴 DON'T TOUCH THIS FILE 🔴🔴 
+ */
+
+/**
+ * `document.getElementById` function will access the elements in the HTML file. 
+ * This is called DOM manipulation
+ * We are manipulating the elements in the HTML file through JS
+ * which is one of the reasons JS eas invented! 
+ */
+try{
+  document.getElementById('_fullName').innerText = fullName
+}
+catch(error){console.error(error)}
+try{
+  document.getElementById('_image').src = image 
+}catch(error){
+  console.error(error)
+  const placeholderAvatar = "https://user-images.githubusercontent.com/8784343/133905455-8b304ea7-815e-4729-af9c-c0252df29e52.jpg";
+  document.getElementById('_image').src = placeholderAvatar;
+}
+try{
+  document.getElementById('_github').innerText = github
+}catch(error){console.error(error)}
+try{
+  document.getElementById('_hobby').innerText = hobby
+}catch(error){console.error(error)}
+try{
+  document.getElementById('_funFact').innerText = funFact
+}catch(error){console.error(error)}
+try{
+  document.getElementById('_familyOrder').innerText = familyOrder
+}catch(error){console.error(error)}
+
+let current = 1
+let height = $('.roles').height()
+let numberDivs = $('.roles').children().length
+let first = $('.roles div:nth-child(1)')
+
+
+
+/**
+ * This function makes the carousel effect every 2000 ms, which is 2 seconds.
+*/
+setInterval(function () {
+  let number = current * -height
+  first.css('margin-top', number + 'px')
+  if (current === numberDivs) {
+    first.css('margin-top', '0px')
+    current = 1
+  } else current++
+}, 2000)
+/**
+ * Secret function
+ */
+
+try{
+  let im = image 
+  try{
+    image = "https://user-images.githubusercontent.com/8784343/133905760-4e6adc27-e3ff-40ac-9999-f0c419b14311.png"
+    setTimeout(function(){
+      document.getElementById('_image').src = image;
+    }, 1000)
+  }
+  catch(error){
+    console.log("✅ Great! you made the image constant")
+  }
+}
+catch(error){}
---
+++ index.js
@@ -0,0 +1,27 @@
+/** Part 1: Variable / constant creation 
+ * In this file you should dfine the following variable with the exact names 
+ * 
+ * 1. fullName      -> assign it a value of your full name
+ * 2. yearOfBirth   -> assign it a value of your year of birth
+ * 3. hobby         -> assign it a value of your favorite hobby
+ * 4. funFact       -> assign it a value of some fun fact about yourself 
+ * 5. image         -> assign it a value of a url of your image or any image that represents you online
+ * 6. familyOrder   -> assign it a value of your family order
+ */
+
+ let fullName = 'Omar ALibrahim'
+ let yearOfBirth = 1996
+ let hobby = 'Singing and making Aziz pissed'
+ let funFact = 'I put ketchup on everryrthing'
+ let familyOrder = '7'
+ const image = 'https://pbs.twimg.com/profile_images/1324664430072909826/Q_GI7x-N_400x400.jpg'
+ 
+/** Part 2: Operations
+ * After creating the variables, modify them again by making them full sentences
+ * 1. fullName      -> change it to: My name is {YOUR_NAME}
+ * 2. yearOfBirth   -> change it to: I am {Years old}, and make sure you calculate your age from your year of birth
+ * 3. hobby         -> change it to: My hobby is {YOUR_HOBBY}
+ * 4. family        -> change it to: My family order is {YOUR_FAMILY_ORDER}
+ */
+
+
